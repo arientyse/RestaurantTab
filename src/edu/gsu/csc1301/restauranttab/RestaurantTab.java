03b351866cc91fb1b8cb6e347bb074ff01e1e8bf
Ariana Tyson <arianatyson@live.com>
2019-10-08 07:20:03 -0400

    Output and Calculations
    Added output and calculations

--- src/edu/gsu/csc1301/restauranttab/RestaurantTab.java
+++ src/edu/gsu/csc1301/restauranttab/RestaurantTab.java
@@ -16,8 +16,40 @@ public class RestaurantTab
 {
 	public static void main(String[] args) 
 	{
-		''
-
+		
+		final double TAX_RATE = 0.075;
+		final double TIP_RATE = 0.18;
+		
+		Scanner in = new Scanner(System.in);
+		
+		//Input
+		
+		System.out.println("What is the server's first name? "); //Asks for the server's first name
+		String fname = in.next();
+		
+		System.out.println("What is the server's last name? "); //Asks for the server's last name
+		String lname = in.next();
+		
+		System.out.println("What was the charge for the meal? "); //Asks for the charge for the meal
+		double meal = in.nextDouble();
+		
+		//Calculations
+		
+		double tax = meal * TAX_RATE; //The tax amount after adjusting for the 7.5% rate
+		double tip = (meal + tax) * TIP_RATE; //The tip amount after adjusting for the 18% tip
+		double total = meal + tax + tip; //The calculated total amount for the customer's meal
+		
+		//Output
+		
+		System.out.println("Restaurant Bill Summary ");
+		System.out.print("Server: " + lname + ", ");
+		System.out.printf("%.1s \n", fname); //Outputs Server's last name and first initial
+		
+		System.out.printf("Meal:    $%.2f \n", meal); //The charge amount for the meal that was inputted
+		System.out.printf("Tax:     $%.2f \n", tax);
+		System.out.printf("Tip:     $%.2f \n", tip);
+		System.out.printf("Total:   $%.2f \n", total);
+		
 	}
 
 }
