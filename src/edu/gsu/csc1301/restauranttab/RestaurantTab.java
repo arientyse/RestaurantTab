a91697f07cecd329797d7a8728af812d411201c7
Ariana Tyson <arianatyson@live.com>
2019-10-08 06:42:20 -0400

    Initial commit

---
+++ src/edu/gsu/csc1301/restauranttab/RestaurantTab.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.gsu.csc1301.restauranttab;
+
+/**
+ * @author Ari
+ *
+ */
+public class RestaurantTab {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module restaurantTab {
+}
